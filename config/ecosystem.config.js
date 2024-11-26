a4026d8c80981804d13d727ddd77246ce2773b3b
Hadrien Froger <hadrien@octree.ch>
2024-11-26 06:10:05 +0000

    chore: update to 0.27.9 version

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -8,8 +8,8 @@ module.exports = {
         interpreter: 'ruby',
         restart_delay: 30000,
         max_restarts: 10,
-        max_memory_restart: '1G',
         min_uptime: 20000,
+        max_size: '10M'
       },
       {
         name: 'sidekiq',
@@ -18,9 +18,28 @@ module.exports = {
         args: 'exec sidekiq',
         interpreter: 'ruby',
         restart_delay: 30000,
-        max_memory_restart: '700M',
+        max_restarts: 10,
         min_uptime: 20000,
-      }
+        max_size: '10M'
+      },
+      {
+        name: 'daily',
+        script: 'daily', 
+        cron_restart: '0 7 * * *', 
+        autorestart: false,
+        log_date_format: 'YYYY-MM-DD HH:mm Z',
+        error_file: './log/daily-err.log',
+        out_file: './log/daily-out.log',
+        max_size: '2M',
+        time: true,
+        max_size: '10M'
+      },
     ],
+    deploy: {
+      production: {
+        post_deploy:
+          'pm2 install pm2-logrotate && pm2 set pm2-logrotate:max_size 10M && pm2 set pm2-logrotate:retain 30',
+      },
+    },
   };
   

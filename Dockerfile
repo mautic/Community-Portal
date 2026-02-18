30f7bf96a73e18c11662bbc6ccac6b06fd30f5ba
Hadrien Froger <hadrien@octree.ch>
2026-02-18 13:56:38 +0000

    chore: update to 0.29.7

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM octree/voca-decidim:0.29-bullseye
-ENV PM2_RUN="decidim,daily,monthly" \
-  ROOT="/home/decidim/app" \
+FROM octree/voca-decidim:0.29-latest
+ENV PM2_RUN="decidim,daily,monthly,external_good_job" \
+  ROOT="/home/decidim" \
   NODE_ENV=development \
   RAILS_ENV=production
 
@@ -9,27 +9,13 @@ WORKDIR $ROOT
 RUN  bundle config set path "$ROOT/vendor" \
   && bundle config set without "development:test" \
   && bundle config set no_cache true \
-  && bundle config set deployment true \
-  && bundle config set frozen true \
-  && rm -rf vendor voca/Gemfile.lock \
-  && rm -rf db/migrate/* 
+  && bundle config set deployment false \
+  && bundle config set frozen false \
+  && rm -rf vendor voca/Gemfile.lock db/migrate/* 
 
 COPY ./contrib/01_mautic_entrypoint /docker-entrypoint.d/01_mautic_entrypoint
 COPY ./contrib/99_recompile /docker-entrypoint.d/99_recompile
 COPY . $ROOT
 
-RUN NODE_MAJOR_VERSION=$(cut -d '.' -f1 /home/decidim/app/.node-version) \
-    && curl -fsSL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x | bash - \
-    && if dpkg -l | grep -qw nodejs; then apt-get purge -y nodejs; fi \
-    && apt-get update -yq \
-    && apt-get install -yq --no-upgrade nodejs nginx libjpeg-dev libpng-dev libtiff-dev libwebp-dev libvips libvips-tools \
-    && export SECRET_KEY_BASE=assets \
-    # Clean installation clutters
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
-    && truncate -s 0 /var/log/*log \
-    && rm -rf node_modules .npm .bundle \
-    && rm -rf /usr/local/bundle/cache 
-
-COPY ./contrib/nginx/nginx.conf /etc/nginx/nginx.conf
-
+RUN bundle install 
 CMD ["pm2-runtime", "start", "config/ecosystem.config.js", "--only", "$PM2_RUN"]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,34 @@
-# frozen_string_literal: true
-
-require "active_support/core_ext/integer/time"
+require "active_support/logger"
+require "rack/attack"
 
 Rails.application.configure do
+  
+  def env_enabled?(env_name, default_value = "disabled")
+    ["true", "1", "enabled"].include? ENV.fetch(env_name, default_value)
+  end
+  
+  def env_present?(env_name)
+    value = ENV.fetch(env_name, "").to_s.strip
+    !value.blank?
+  end
+  
+  # Disable Rack Attack, let Nginx handle this. 
+  Rack::Attack.enabled = false
+
   # Settings specified here will take precedence over those in config/application.rb.
-  config.deface.enabled = ENV["DEFACE_ENABLED"] == "true"
+  config.after_initialize do 
+    if ActiveRecord::Base.connected?
+      if ::Decidim::Organization.count > 0 
+        ::Decidim::Organization.all.pluck(:host).each do |host|
+          Rails.application.config.hosts << host
+        end
+      else
+        Rails.application.config.hosts = /.*/
+      end
+    end
+  end
+
+  config.deface.enabled = env_enabled?(ENV.fetch('DEFACE_ENABLED', "disabled"))
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -16,53 +40,69 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.active_storage.variant_processor = :vips
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = env_enabled?("RAILS_SERVE_STATIC_FILES")
 
-  # Compress CSS using a preprocessor.
-  #
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = ENV["RAILS_ASSET_HOST"] if ENV["RAILS_ASSET_HOST"].present?
+  # config.action_controller.asset_host = 'http://assets.example.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = Rails.application.secrets.dig(:storage, :provider) || :local
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = ENV.fetch("STORAGE_PROVIDER", "local").to_sym
 
-  # Mount Action Cable outside main process or domain.
+  # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = env_enabled?("DECIDIM_FORCE_SSL")
 
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = %w(debug info warn error fatal).include?(ENV["RAILS_LOG_LEVEL"]) ? ENV["RAILS_LOG_LEVEL"] : :info
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = ENV.fetch("DECIDIM_LOG_LEVEL", "warn").to_sym
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [:request_id]
+  config.log_tags = [ :request_id ]
 
-  # Use Memcached in production.
-  config.cache_store = :mem_cache_store
+  # Use a different cache store in production.
+  if env_present?("CACHE_ADAPTER")
+    if ENV.fetch("CACHE_ADAPTER", "") == "memcached"
+      # See https://github.com/petergoldstein/dalli/wiki/Using-Dalli-with-Rails#cache-store
+      servers = ENV.fetch("MEMCACHE_SERVERS", "localhost:11211").split(",").map(&:strip)
+      config.cache_store = :mem_cache_store, *servers
+    end
+  end
+  # Set session store activerecord-session_store keys if loaded
+  if Gem.loaded_specs.has_key?('activerecord-session_store')
+    config.after_initialize do
+      Rails.application.config.session_store :active_record_store, :key => '_voca_session'
+    end
+  end
+  
+  if env_present?("QUEUE_ADAPTER")
+    # Use a real queuing backend for Active Job (and separate queues per environment)
+    if ENV.fetch("QUEUE_ADAPTER", "default") == "sidekiq" && ENV["REDIS_URL"].present?
+      config.active_job.queue_adapter = :sidekiq
+    end
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = ENV["QUEUE_ADAPTER"] if ENV["QUEUE_ADAPTER"].present?
-  # config.active_job.queue_name_prefix = "app_production"
+    if ENV.fetch("QUEUE_ADAPTER", "default") == "good_job"
+      config.active_job.queue_adapter = :good_job
+    end
+  end
 
   config.action_mailer.perform_caching = false
 
@@ -73,60 +113,72 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.i18n.raise_on_missing_translations = false
+
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Log disallowed deprecations.
-  config.active_support.disallowed_deprecation = :log
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = env_enabled?("SMTP_RAISE_DELIVERY_ERRORS", "enabled")
 
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
+  config.action_mailer.smtp_settings = {}
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = Logger::Formatter.new
-  config.action_mailer.smtp_settings = {
-    :address => Rails.application.secrets.smtp_address,
-    :port => Rails.application.secrets.smtp_port,
-    :authentication => Rails.application.secrets.smtp_authentication,
-    :user_name => Rails.application.secrets.smtp_username,
-    :password => Rails.application.secrets.smtp_password,
-    :domain => Rails.application.secrets.smtp_domain,
-    :enable_starttls_auto => Rails.application.secrets.smtp_starttls_auto,
-    :openssl_verify_mode => "none"
-  }
+  if env_present?("SMTP_ADDRESS")
+    config.action_mailer.smtp_settings[:address] = ENV.fetch("SMTP_ADDRESS", "")
+  end
+
+  if env_present?("SMTP_PORT")
+    config.action_mailer.smtp_settings[:port] = ENV.fetch("SMTP_PORT", "587").to_i
+  end
+  
+  if env_present?("SMTP_DOMAIN")
+    config.action_mailer.smtp_settings[:domain] = ENV.fetch("SMTP_DOMAIN", "")
+  end
+
+  if env_present?("SMTP_SSL")
+    config.action_mailer.smtp_settings[:ssl] = env_enabled?("SMTP_SSL", "disabled")
+  end
+
+  if env_present?("SMTP_TLS")
+    config.action_mailer.smtp_settings[:tls] = env_enabled?("SMTP_TLS", "disabled")
+  end
+
+  if env_present?("SMTP_STARTTLS_AUTO")
+    config.action_mailer.smtp_settings[:enable_starttls_auto] = env_enabled?("SMTP_STARTTLS_AUTO", "disabled")
+  end
+
+  if env_present?("SMTP_VERIFY_MODE")
+    verify_mode = ENV.fetch("SMTP_VERIFY_MODE", "none") == "peer" ? "peer" : "none"
+    config.action_mailer.smtp_settings[:openssl_verify_mode] = verify_mode
+  end
+
+  if env_present?("SMTP_OPEN_TIMEOUT")
+    config.action_mailer.smtp_settings[:open_timeout] = ENV.fetch("SMTP_OPEN_TIMEOUT", "6000").to_i
+  end
+  
+  if env_present?("SMTP_READ_TIMEOUT")
+    config.action_mailer.smtp_settings[:read_timeout] = ENV.fetch("SMTP_READ_TIMEOUT", "6000").to_i
+  end
+
+  if env_present?("SMTP_AUTHENTICATION")
+    authentication_type = ENV.fetch("SMTP_AUTHENTICATION", "plain")
+    case authentication_type
+    when "plain", "login", "cram_md5"
+      config.action_mailer.smtp_settings[:authentication] = authentication_type
+      config.action_mailer.smtp_settings[:user_name] = ENV.fetch("SMTP_USERNAME", "") if env_present?("SMTP_USERNAME")
+      config.action_mailer.smtp_settings[:password] = ENV.fetch("SMTP_PASSWORD", "") if env_present?("SMTP_PASSWORD")
+    else
+      raise "Invalid SMTP authentication type: #{authentication_type}. Valid options are: plain, login, cram_md5."
+    end
+  end
 
   # Use a different logger for distributed setups.
-  # require "syslog/logger"
+  # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
-
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger = ActiveSupport::Logger.new($stdout)
-    logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
-  end
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.logger = ActiveSupport::Logger.new(STDOUT)
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # Inserts middleware to perform automatic connection switching.
-  # The `database_selector` hash is used to pass options to the DatabaseSelector
-  # middleware. The `delay` is used to determine how long to wait after a write
-  # to send a subsequent read to the primary.
-  #
-  # The `database_resolver` class is used by the middleware to determine which
-  # database is appropriate to use based on the time delay.
-  #
-  # The `database_resolver_context` class is used by the middleware to set
-  # timestamps for the last write to the primary. The resolver uses the context
-  # class timestamps to determine how long to wait before reading from the
-  # replica.
-  #
-  # By default Rails will store a last write timestamp in the session. The
-  # DatabaseSelector middleware is designed as such you can define your own
-  # strategy for connection switching and pass that into the middleware through
-  # these configuration options.
-  # config.active_record.database_selector = { delay: 2.seconds }
-  # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
-  # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 end
